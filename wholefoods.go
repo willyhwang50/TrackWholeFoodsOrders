a6409cbbf7ed74eb575be436c59c570579d86f68
Will Hwang <willyhwang50@gmail.com>
2021-03-02 22:36:43 -0600

    latest commit

--- wholefoods.go
+++ wholefoods.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -23,14 +24,38 @@ import (
 	"google.golang.org/api/gmail/v1"
 )
 
-// GrandTotal Indicator.
-const GrandTotal string = "Grand total:"
+// Indicators for extracting features.
+const (
+	GrandTotal   = "Grand total:"
+	DeliveryTime = "delivery time:"
+	OrderID      = "Details Order"
+)
 
-// DeliveryTime Indicator.
-const DeliveryTime string = "delivery time:"
+// String Longform used for Parsing Strings to type Time
+const (
+	longform  = "Jan 2, 2006 at 3:04pm (MST)"
+	shortform = "2006-Jan-02"
+)
 
-// OrderID Indicator.
-const OrderID string = "Details Order"
+const query = "{from:order-update@amazon.com} 'your delivery is complete' 'Grand total'"
+
+// Monthmap connects abbreviated name of month to corresponding int
+var Monthmap = map[string]string{
+	"Jan":      "01",
+	"January":  "01",
+	"Feb":      "02",
+	"February": "02",
+	"Mar":      "03",
+	"Apr":      "04",
+	"May":      "05",
+	"Jun":      "06",
+	"Jul":      "07",
+	"Aug":      "08",
+	"Sep":      "09",
+	"Oct":      "10",
+	"Nov":      "11",
+	"Dec":      "12",
+}
 
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
@@ -87,16 +112,20 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-// Order has properties ID, Date, Total.
+// Order TYPE has properties ID, Date, Total.
 type Order struct {
 	OrdNum     string
-	OrdDate    time.Time
+	OrdDate    string
 	GrandTotal float64
 }
 
-//ConvtoTime string to type time.Time.
-func ConvtoTime(t string) time.Time {
-	const shortForm = "2006-Jan-02"
+//GetSummary of the properties.
+func (order Order) GetSummary() {
+	fmt.Println(order.OrdNum, order.OrdDate, order.GrandTotal)
+}
+
+// ConvtoTime casts dates written in strings to type time.Time.
+func ConvtoTime(t string) string {
 	rawtime := strings.Fields(t)[1:]
 	month := rawtime[0][:3]
 	day := strings.Trim(rawtime[1], ",")
@@ -108,16 +137,12 @@ func ConvtoTime(t string) time.Time {
 		day = "0" + day
 	}
 	year := rawtime[2]
-	shortdate := year + "-" + month + "-" + day
-	ordtime, err := time.Parse(shortForm, shortdate)
-	if err != nil {
-		log.Fatal("cannot convert to time obj", err)
-	}
-	return ordtime
+	shortdate := year + "-" + Monthmap[month] + "-" + day
+	return shortdate
 }
 
-// ExtractFeat Id, Date, Grand Total given email body as string.
-func ExtractFeat(s *[]byte) (id string, date time.Time, tot float64) {
+// ExtractFeat finds the order Id, Date, Grand Total given email body as string.
+func ExtractFeat(s *[]byte) (id string, date string, tot float64) {
 	info := strings.Fields(string(*s))
 	var idFound bool = false
 	var dtFound bool = false
@@ -147,7 +172,7 @@ func ExtractFeat(s *[]byte) (id string, date time.Time, tot float64) {
 	return id, date, tot
 }
 
-// GetOrderFeats message response to create an array of Order struct.
+// GetOrderFeats creates an array of Order structs.
 func GetOrderFeats(user string, srv *gmail.Service, r *gmail.ListMessagesResponse) []Order {
 	Orders := []Order{}
 	for _, msg := range r.Messages {
@@ -167,62 +192,42 @@ func GetOrderFeats(user string, srv *gmail.Service, r *gmail.ListMessagesRespons
 	return Orders
 }
 
-// GetPassword to db.
-func GetPassword(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("env not loaded", err)
-	}
-	return os.Getenv(key)
-}
-
-// InsertOrder to db.
+// InsertOrder to Database.
 func InsertOrder(Orders []Order, db *sql.DB) {
 	for i, ord := range Orders {
 		id := ord.OrdNum
-		yearnum, month, daynum := ord.OrdDate.Date()
-		year := strconv.Itoa(yearnum)
-		day := strconv.Itoa(daynum)
-		DateString := year + "-" + month.String() + "-" + day
+		DateString := ord.OrdDate
 		total := fmt.Sprintf("%f", ord.GrandTotal)
 		values := "'" + id + "', '" + DateString + "', " + total
 		sql := "INSERT INTO WholeFoods(order_id, order_date, grand_total) VALUES" +
 			"(" + values + ")"
 		_, err := db.Exec(sql)
 		if err != nil {
-			log.Fatal("cannot add %d th row to database", i, err)
+			log.Printf("cannot add %d th row to database", i)
+			log.Fatal(err)
 		}
 	}
 }
 
-func main() {
-	//read credientials
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
+// GetQuery forms a SQL Query using lastupdate
+func GetQuery(query string, lastupdate string) string {
+	re := regexp.MustCompile("-")
+	lastupdate = re.ReplaceAllString(lastupdate, "/")
+	re = regexp.MustCompile("[A-Za-z]+")
+	lastupdate = re.ReplaceAllString(lastupdate, Monthmap[lastupdate[5:8]])
+	NewQuery := query + " after:" + lastupdate
+	return NewQuery
+}
 
-	// create a new client using credentials
-	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
-
-	//instantiate a gmail service
-	srv, err := gmail.New(client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve Gmail client: %v", err)
-	}
-
-	//retrieve mails
-	user := "me"
-	r, err := srv.Users.Messages.List(user).IncludeSpamTrash(
-		false).MaxResults(10).Q("{from:order-update@amazon.com} 'your delivery is complete' 'Grand total'").Do()
+// ReadData retrieves relevant Emails.
+func ReadData(srv *gmail.Service, lastupdate string) []Order {
+	const user = "me"
+	NewQuery := GetQuery(query, lastupdate)
+	r, err := srv.Users.Messages.List(user).IncludeSpamTrash(false).MaxResults(10).Q(NewQuery).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve mails: %v", err)
 	}
-	fmt.Println(len(r.Messages))
+	fmt.Println("The length of the list is", len(r.Messages))
 
 	//extract features from emails
 	Orders := GetOrderFeats(user, srv, r)
@@ -230,28 +235,272 @@ func main() {
 		log.Fatal("Orders is empty")
 	}
 
-	//marshall array of orders into json
+	//Save Newly extracted Data as temporary json file
 	Ordfile, err := json.Marshal(Orders)
 	if err != nil {
 		log.Fatal("Can't marshall json", err)
 	}
-
-	//save json file for later
 	ioutil.WriteFile("Orders.json", Ordfile, os.ModePerm)
 
-	//retrieve db password
-	password := GetPassword("password")
-	if len(password) == 0 {
-		log.Fatal("cannot load password")
-	}
+	return Orders
+}
 
-	//Instantiate Database Driver
-	db, err := sql.Open("mysql", "root:"+password+"@tcp(127.0.0.1:3306)/sys")
-	if err != nil {
-		log.Fatal(err)
-	}
+//UpdateDB with new records after the lastupdate date
+func UpdateDB(srv *gmail.Service, lastupdate string, db *sql.DB) {
+
+	// Retrieve Data from Email Address
+	Orders := ReadData(srv, lastupdate)
 
 	//insert data into db
 	InsertOrder(Orders, db)
 	fmt.Println("successfully uploaded data")
 }
+
+//RetrieveOrders from mySQL Database.
+func RetrieveOrders(db *sql.DB, cond *Conditions) {
+	sql := (*cond).GetQuery()
+	fmt.Println(sql)
+	rows, err := db.Query(sql)
+	if err != nil {
+		log.Fatal("cannot load data from mySQL database")
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var ordData Order
+		var id string
+		err = rows.Scan(&id, &ordData.OrdNum, &ordData.OrdDate, &ordData.GrandTotal)
+		if err != nil {
+			log.Fatal("cannot retrieve data", err)
+		}
+		ordData.GetSummary()
+	}
+}
+
+// Conditions is a struct which contains information for writing a select query
+type Conditions struct {
+	start   string
+	end     string
+	lb      string
+	ub      string
+	numrows string
+}
+
+// GetConditions retrieves summary of the conditions as a string
+func (c Conditions) GetConditions() string {
+	cond := "Date: " + c.start + "~" + c.end + "/ "
+	cond += "Total amount: " + c.lb + " ~ " + c.ub + "/ "
+	cond += "Number of Rows: " + c.numrows + "/ "
+	return cond
+}
+
+//GetQuery function creates a Query for retrieving data from mySQL
+func (c Conditions) GetQuery() string {
+	Query := "SELECT * from Wholefoods where '" + c.start + "' < order_date and order_date < '" + c.end + "' and " + c.lb + " < grand_total and grand_total < " + c.ub + " limit " + c.numrows
+	return Query
+}
+
+//CreateView collects conditions for desired data
+func CreateView(srv *gmail.Service, db *sql.DB) {
+	Condmap := map[int]bool{
+		1: false,
+		2: false,
+		3: false,
+	}
+	condInit := Conditions{
+		start:   "2021-01-01",
+		end:     "2021-05-01",
+		lb:      "0.0",
+		ub:      "100000",
+		numrows: "100",
+	}
+	cond := condInit
+	var cat int
+CondPanel:
+	for {
+		fmt.Println("Specify conditions for the data you want to view: ")
+		fmt.Println("Current Conditions are: ", cond.GetConditions())
+		fmt.Println("Add Conditions of...")
+		fmt.Println("1. Date")
+		fmt.Println("2. Total Amount")
+		fmt.Println("3. Number of Rows")
+		fmt.Println("4. Retrieve All data")
+		fmt.Println("5. Retrieve With Current Condition")
+		fmt.Println("6. Return to Main")
+		fmt.Scanln(&cat)
+		switch cat {
+		case 1:
+			if Condmap[1] {
+				var resp string
+				fmt.Println("Dates Already Specified. Do you want to Override? yes/no")
+				fmt.Scanln(&resp)
+				if resp == "no" {
+					continue
+				}
+			}
+			var start string
+			var end string
+			fmt.Println("Enter Starting Date: (format yyyy-mm-dd)")
+			fmt.Scanln(&start)
+			cond.start = start
+			fmt.Println("Enter End Date: (format yyyy-mm-dd)")
+			fmt.Scanln(&end)
+			cond.end = end
+			Condmap[1] = true
+		case 2:
+			if Condmap[2] {
+				var resp string
+				fmt.Println("Total Amount Already Specified. Do you want to Override? yes/no")
+				fmt.Scanln(&resp)
+				if resp == "no" {
+					continue
+				}
+			}
+			var lb string
+			var ub string
+			fmt.Println("1. Greater Than")
+			fmt.Scanln(&lb)
+			fmt.Println("2. Less Than")
+			fmt.Scanln(&ub)
+			Condmap[2] = true
+		case 3:
+			if Condmap[3] {
+				var resp string
+				fmt.Println("Number of Rows Already Specified. Do you want to Override? yes/no")
+				fmt.Scanln(&resp)
+				if resp == "no" {
+					continue
+				}
+			}
+			var numrows string
+			fmt.Println("How Many Rows do you want?")
+			fmt.Scanln(&numrows)
+			Condmap[3] = true
+		case 4:
+			fmt.Println("Retrieving All data")
+			RetrieveOrders(db, &condInit)
+		case 5:
+			fmt.Println("Retrieving Data with conditions: ", cond.GetConditions())
+			RetrieveOrders(db, &cond)
+			break CondPanel
+		case 6:
+			break CondPanel
+		default:
+			fmt.Println("Not a valid category")
+			continue
+		}
+	}
+	return
+}
+
+func main() {
+	// Welcome Message
+	fmt.Println("Welcome! Initiating...")
+
+	//Read Gmail Credientials
+	fmt.Println("Reading Credentials...")
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	fmt.Println("Creating Client...")
+
+	// Create a new client using credentials
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	//instantiate a gmail service
+	fmt.Println("Instantiating Gmail Service...")
+	srv, err := gmail.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+	}
+
+	//Get Environmental Variables
+	fmt.Println("Loading Environmental Variables...")
+	godotenv.Load(".env")
+
+	//retrieve db password
+	password := os.Getenv("password")
+	if len(password) == 0 {
+		log.Fatal("cannot load password")
+	}
+
+	//Instantiate Database Driver
+	fmt.Println("Connecting to mySQL Database")
+	db, err := sql.Open("mysql", "root:"+password+"@tcp(127.0.0.1:3306)/sys")
+	if err != nil {
+		log.Fatal("cannot instantaite database driver", err)
+	}
+
+	//Check if Database should be Updated
+	LastUpdate, found := os.LookupEnv("LastUpdate")
+	if !found {
+		LastUpdate = "2021-Jan-01"
+		os.Setenv("LastUpdate", LastUpdate)
+	}
+	fmt.Println("Your last update is on ", LastUpdate)
+	LastUpdateTime, err := time.Parse(shortform, LastUpdate)
+	if err != nil {
+		log.Fatal("cannot parse lastupdate date to time")
+	}
+	TimeNow := time.Now()
+	if !LastUpdateTime.Equal(TimeNow) {
+		fmt.Println("You have not updated your database for", TimeNow.Sub(LastUpdateTime).Hours(), "hours")
+	}
+	fmt.Print("Do you want to update your database?: yes/no (lowercase)")
+	var update string
+	fmt.Scanln(&update)
+
+UpdateQ:
+	for {
+		switch update {
+		case "yes":
+			UpdateDB(srv, LastUpdate, db)
+			fmt.Println("Update is complete")
+			break UpdateQ
+		case "no":
+			fmt.Println("Not Updating Database")
+			break UpdateQ
+		default:
+			fmt.Println("Not a proper command. Type 'yes' or 'no'")
+			fmt.Scanln(&update)
+		}
+	}
+
+	fmt.Println("Directing you to Control Panel")
+	fmt.Println("...........................................................")
+
+	var action int
+ActionPanel:
+	for {
+		//Choose Action
+		fmt.Println("Choose Options: ")
+		fmt.Println("1: View Order Records")
+		fmt.Println("2: Edit Order Records")
+		fmt.Println("3: Get Stats")
+		fmt.Println("4: Quit")
+		fmt.Scanln(&action)
+		switch action {
+		case 1:
+			fmt.Println("Directing to View...")
+			CreateView(srv, db)
+		case 2:
+			fmt.Println("Directing to Edit...")
+		case 3:
+			fmt.Println("Directing to Stats...")
+		case 4:
+			fmt.Println("Bye bye")
+			break ActionPanel
+		default:
+			fmt.Println("Not a valid choice")
+			continue
+		}
+	}
+}
+
+// Update Once then view directs to db
+// unmarshall json file (temp)
